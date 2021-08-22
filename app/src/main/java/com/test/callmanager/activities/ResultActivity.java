1dc27db1c4865f5199d4c36d616bbf8d0eca0d0c
Mohammad Yazdi <mohammadyt1379@gmail.com>
2021-08-22 20:44:14 +0430

    Change view ResultActivity

--- app/src/main/java/com/test/callmanager/activities/ResultActivity.java
+++ app/src/main/java/com/test/callmanager/activities/ResultActivity.java
@@ -1,17 +1,26 @@
 package com.test.callmanager.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.test.callmanager.R;
 
 public class ResultActivity extends AppCompatActivity {
 
     TextView tvSubmitResult;
+    TextView tvSituationMenu;
+    TextView tvPriorityMenu;
+
+    ConstraintLayout clPrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,7 @@ public class ResultActivity extends AppCompatActivity {
 
         findViews();
         Configuration();
+        setUpMenu();
 
     }
 
@@ -27,6 +37,10 @@ public class ResultActivity extends AppCompatActivity {
     private void findViews(){
 
         tvSubmitResult=findViewById(R.id.tv_submit_meet);
+        tvSituationMenu=findViewById(R.id.tv_situation_drop_down);
+        tvPriorityMenu=findViewById(R.id.tv_priority_drop_down);
+
+        clPrice=findViewById(R.id.cl_price);
     }
 
     private void Configuration() {
@@ -38,5 +52,79 @@ public class ResultActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+    private void setUpMenu(){
+
+        PopupMenu situationMenu=new PopupMenu(this,tvSituationMenu);
+        situationMenu.getMenu().add("خاموش بودن تلفن");
+        situationMenu.getMenu().add("برنداشتن تلفن");
+        situationMenu.getMenu().add("عدم انجام تراکنش");
+        situationMenu.getMenu().add("تراکنش موفق");
+
+
+        situationMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getTitle().toString().equals("تراکنش موفق")){
+                    clPrice.setVisibility(View.VISIBLE);
+
+                }else{
+                    clPrice.setVisibility(View.GONE);
+                }
+
+                tvSituationMenu.setText(item.getTitle());
+
+
+                return false;
+            }
+        });
+
+        tvSituationMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                situationMenu.show();
+            }
+        });
+
+
+        PopupMenu priorityMenu=new PopupMenu(this,tvPriorityMenu);
+        priorityMenu.getMenu().add("1");
+        priorityMenu.getMenu().add("2");
+        priorityMenu.getMenu().add("3");
+        priorityMenu.getMenu().add("4");
+        priorityMenu.getMenu().add("5");
+        priorityMenu.getMenu().add("6");
+        priorityMenu.getMenu().add("7");
+        priorityMenu.getMenu().add("8");
+        priorityMenu.getMenu().add("9");
+        priorityMenu.getMenu().add("10");
+
+
+        priorityMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                tvPriorityMenu.setText(item.getTitle());
+
+                return false;
+            }
+        });
+
+
+        tvPriorityMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                priorityMenu.show();
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
